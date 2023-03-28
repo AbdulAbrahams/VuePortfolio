810a380b80469eed43d812868b24a920a25e5709
AbdulAbrahams <114492995+AbdulAbrahams@users.noreply.github.com>
2023-03-28 15:01:41 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,10 @@
 
 <script>
 
-import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 export default{
   name: 'App',
-  components: {Navbar,Footer},
+  components: {Footer},
 }
 </script>
 
