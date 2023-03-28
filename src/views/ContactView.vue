5aa814798d3c5e916a0cbb95071643c50cfb4b47
AbdulAbrahams <114492995+AbdulAbrahams@users.noreply.github.com>
2023-03-28 15:18:10 +0200

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,7 @@
 <template>
+      <div class="space"></div>
         <div class="contactsec container-lg" id="Contact">
-      <h1 class="contact my-5" style="padding-bottom: 6%; color: red;">Contact Me</h1>
+      <h1 class="contact" style="padding-bottom: 6%; color: red;">Contact Me</h1>
       <form action="https://formspree.io/f/xzbwjdvo" method="POST" class="form" style="padding-bottom: 7%;">
           <div class="row">
             <div class="col">
